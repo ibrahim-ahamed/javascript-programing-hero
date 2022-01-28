15fa7d67e46a58df0be4d1bd9f1b2068510120d5
ibrahim Ahmed <bdibrahimahmed1231@gmail.com>
2022-01-28 10:17:19 +0600

    condition add

---
+++ condition.js
@@ -0,0 +1,30 @@
+var pocketMony = 70;
+if(pocketMony > 50){
+    console.log('Ami Cha Khamu');
+}else{
+    console.log('ami khamu na');
+};
+
+var iphonePrice = 150000;
+var myBudget = 70000;
+
+if (iphonePrice < myBudget){
+    console.log('Ami Iphone Kinmo');
+}else{
+    console.log('Ami google pixy phone kinmo');
+};
+if (iphonePrice > myBudget){
+    console.log('Ami Iphone Kinmo');
+}else{
+    console.log('Ami google pixy phone kinmo');
+};
+if (iphonePrice == myBudget){
+    console.log('Ami Iphone Kinmo');
+}else{
+    console.log('Ami google pixy phone kinmo');
+};
+if (iphonePrice != myBudget){
+    console.log('Ami Iphone Kinmo');
+}else{
+    console.log('Ami google pixy phone kinmo');
+};
