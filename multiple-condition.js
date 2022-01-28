f3310c55163863e5a749ba14627c0231cb4d7e15
ibrahim Ahmed <bdibrahimahmed1231@gmail.com>
2022-01-28 11:40:55 +0600

    multiple condition add

---
+++ multiple-condition.js
@@ -0,0 +1,27 @@
+var govJob = false;
+var ghorVari = false;
+var monySave = 250000;
+
+// if (govJob == true && ghorVari == true && monySave > 200000){
+//     console.log('Ami Biya kormo');
+// }else{
+//     console.log('ami biya kormo na');
+// }
+
+// if ((govJob == true && ghorVari == true) || monySave > 200000){
+//     console.log('Ami Biya kormo');
+// }else{
+//     console.log('ami biya kormo na');
+// }
+
+// if ((govJob == true || ghorVari == true) && monySave > 200000){
+//     console.log('Ami Biya kormo');
+// }else{
+//     console.log('ami biya kormo na');
+// }
+
+if (govJob == true || ghorVari == true || monySave > 200000){
+    console.log('Ami Biya kormo');
+}else{
+    console.log('ami biya kormo na');
+}
