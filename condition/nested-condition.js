d31e6cbefdbe2753de80153cee9149a5ddeb7e3a
ibrahim Ahmed <bdibrahimahmed1231@gmail.com>
2022-01-28 20:09:28 +0600

    while loop add

---
+++ condition/nested-condition.js
@@ -0,0 +1,19 @@
+
+var toast = 30;
+var milk = 40;
+var bed = 35;
+var masi = true;
+var myBydget = 100;
+
+if(toast < myBudget){
+    console.log(' ami biskot diye cha khabo');
+}
+else if(milk > myBudget){
+    console.log('ami dod cha khabo');
+}
+else if(bed > myBudget){
+        console.log('ami bed diye cha khabo');
+}
+else{
+    console.log('ami cha khamo na');
+}
---
+++ recap-loop/while-loop.js
@@ -0,0 +1,7 @@
+var morgirRoast = 0;
+
+while(morgirRoast <= 100){
+    console.log('ami morgir roast diye polaw khabo');
+    morgirRoast++;
+    console.log(morgirRoast);
+}
