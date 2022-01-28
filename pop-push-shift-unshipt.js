4c63442be034a7151a825139e68d204cf5ef4c95
ibrahim Ahmed <bdibrahimahmed1231@gmail.com>
2022-01-28 09:20:44 +0600

    push pop shift unshift add

---
+++ pop-push-shift-unshipt.js
@@ -0,0 +1,16 @@
+var lastBench = ['ibrahim', 'totol', 'brooni', 'bondhon' ];
+console.log(lastBench);
+// lastBench.push('Abubakar');
+// console.log(lastBench);
+// lastBench.pop()
+// lastBench.pop()
+// lastBench.pop()
+// lastBench.pop()
+// var lastShow = lastBench.pop()
+// console.log(lastBench)
+// console.log(lastShow)
+// lastBench.unshift('abubakar');
+// console.log(lastBench);
+lastBench.shift();
+console.log(lastBench);
+
