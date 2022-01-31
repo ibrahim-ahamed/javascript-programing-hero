c00c00cb2bb18d3f91d50fb7cc6ebe9a323044d7
ibrahim Ahmed <bdibrahimahmed1231@gmail.com>
2022-01-31 06:27:39 +0600

    while loop example

---
+++ recap-loop/while-loop-example.js
@@ -0,0 +1,21 @@
+var num = 0;
+
+// while(num <= 20){
+//     console.log(num);
+//     num++;
+// };
+
+// odd number 
+
+// while(num <= 20){
+//     console.log(num);
+//     num += 2;
+// };
+
+
+// even number 
+
+while(num <= 20){
+    console.log(num);
+    num += 2;
+};
