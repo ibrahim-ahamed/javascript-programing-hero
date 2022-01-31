416b6adb19fd62a44d1f72b60dde52945ce173c2
ibrahim Ahmed <bdibrahimahmed1231@gmail.com>
2022-01-31 15:04:55 +0600

    breack contenio

---
+++ explore-function-object/breack-contineo.js
@@ -0,0 +1,15 @@
+// var i = 1;
+// while(i <= 20){
+//     if(i == 10){
+//         break;
+//     }
+//     console.log(i);
+//     i++;
+// };
+
+for(var i = 1; i < 10; i++){
+    if(i > 6){
+        continue;
+    }
+    console.log(i)
+}
