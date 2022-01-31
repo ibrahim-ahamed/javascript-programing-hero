e830fc2063b516c60b5ca8201327705da343b6d1
ibrahim Ahmed <bdibrahimahmed1231@gmail.com>
2022-01-31 08:09:44 +0600

    explore function objact folder add and reacapt variabla array loop

---
+++ explore-function-object/reacap.js
@@ -0,0 +1,32 @@
+// 1. variable 
+var favoriteBook = 'hatekolome java script';
+
+// 2. array
+var bookList = ['full stack', 'hablu', 'hate kolome javascript'];
+
+
+console.log(bookList);
+bookList[1] = 'ict';
+console.log(bookList);
+bookList.push('programing er choddo gusti');
+console.log(bookList);
+bookList.pop();
+console.log(bookList);
+bookList.unshift('programing er choddo gusti');
+console.log(bookList);
+bookList.shift();
+console.log(bookList);
+
+// 3. loop
+
+// while loop
+var i = 0;
+while(i < 10){
+    console.log(i);
+    i++;
+};
+// for loop
+
+for(var i = 0; i < 10; i++){
+    console.log(i);
+};
