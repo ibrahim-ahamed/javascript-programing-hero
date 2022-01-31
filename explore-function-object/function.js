2e1c55f5a4ffe6c3241f5cdd74f06a3963ee7a7f
ibrahim Ahmed <bdibrahimahmed1231@gmail.com>
2022-01-31 08:33:00 +0600

    function add

---
+++ explore-function-object/function.js
@@ -0,0 +1,18 @@
+function batKhamo(){
+    console.log('ami akhon vat khamo');
+};
+batKhamo()
+
+console.log('ami akhon ami akhon kaj kormo');
+console.log('ami akhon ami akhon kaj kormo');
+console.log('ami akhon ami akhon kaj kormo');
+console.log('ami akhon ami akhon kaj kormo');
+console.log('ami akhon ami akhon kaj kormo');
+batKhamo()
+
+console.log('ami akhon ami akhon kaj kormo');
+console.log('ami akhon ami akhon kaj kormo');
+console.log('ami akhon ami akhon kaj kormo');
+console.log('ami akhon ami akhon kaj kormo');
+console.log('ami akhon ami akhon kaj kormo');
+batKhamo()
