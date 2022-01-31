83e27cbc6e1a62d50729cd9c2bdf7be0df1471f7
ibrahim Ahmed <bdibrahimahmed1231@gmail.com>
2022-01-31 12:48:11 +0600

    switch case

---
+++ explore-function-object/witchcase.js
@@ -0,0 +1,17 @@
+var color = 'yellow'
+switch(color){
+    case 'red':
+        console.log('red')
+    break;
+    case 'white':
+        console.log('white')
+    break;
+    case 'black':
+        console.log('black')
+    break;
+    case 'yellow':
+        console.log('yellow')
+    break;
+    default:
+        console.log('ornge')
+}
