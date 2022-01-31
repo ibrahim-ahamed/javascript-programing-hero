f97d644d6a17fbbe48524240fa41d5b70d23e604
ibrahim Ahmed <bdibrahimahmed1231@gmail.com>
2022-01-31 06:58:26 +0600

    for loop

---
+++ recap-loop/for-loop.js
@@ -0,0 +1,13 @@
+// for(var i = 1; i <= 100; i++){
+//     console.log(i +' '+ "Sahina I Love You");
+// };
+
+// odd Number
+for(var i = 1; i <= 100; i+= 2){
+    console.log(i +' '+ "Sahina I Love You");
+};
+
+// even number
+for(var i = 0; i <= 100; i+=2){
+    console.log(i +' '+ "Sahina I Love You");
+};
