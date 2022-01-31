54119d14076acbcf6b7a3f97bd4d69234202ea2b
ibrahim Ahmed <bdibrahimahmed1231@gmail.com>
2022-01-31 07:26:57 +0600

    array looping

---
+++ recap-loop/array-loop.js
@@ -0,0 +1,13 @@
+var number = ['ami', 'sahina', 'ke', 'valobashi'];
+
+// for(var i = 0; i <= 7; i++){
+//     var element = number[i];
+//     console.log(element)
+// }
+
+// for loop
+for(var i = 0; i < number.length; i++){
+    var element = number[i];
+    console.log(element);
+};
+
