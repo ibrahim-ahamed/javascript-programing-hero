193ae9725174de0959723be75934586c4cd7f111
ibrahim Ahmed <bdibrahimahmed1231@gmail.com>
2022-01-31 09:08:54 +0600

    function parametar

---
+++ explore-function-object/parameter.js
@@ -0,0 +1,9 @@
+function singaraKhamu(mony){
+    console.log('Amake' + ' ' + mony + ' ' + 'Taka Diche Shingara Khaite');
+    console.log('Mama Amake Shingara den');
+    var singaraPrice = 5;
+    var singaraquantity = mony / singaraPrice;
+    return(singaraquantity);
+}
+var totalSingara = singaraKhamu(500);
+console.log('Ai nen Apner Singara' + ' ' + totalSingara + ' ' + 'Ta')
