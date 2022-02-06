75e949f772454dd07ddb06b9769c1ab948df2cb7
ibrahim Ahmed <bdibrahimahmed1231@gmail.com>
2022-02-06 07:38:18 +0600

    nested condition

--- condition/nested-condition.js
+++ condition/nested-condition.js
@@ -1,19 +1,38 @@
 
-var toast = 30;
-var milk = 40;
-var bed = 35;
-var masi = true;
-var myBydget = 100;
+// var toast = 30;
+// var milk = 40;
+// var bed = 35;
+// var masi = true;
+// var myBydget = 100;
 
-if(toast < myBudget){
-    console.log(' ami biskot diye cha khabo');
+// if(toast < myBudget){
+//     console.log(' ami biskot diye cha khabo');
+// }
+// else if(milk > myBudget){
+//     console.log('ami dod cha khabo');
+// }
+// else if(bed > myBudget){
+//         console.log('ami bed diye cha khabo');
+// }
+// else{
+//     console.log('ami cha khamo na');
+// }
+
+
+var myResult = 32;
+
+if (myResult >= 80){
+    console.log('Ami A+ Paise')
 }
-else if(milk > myBudget){
-    console.log('ami dod cha khabo');
+else if(myResult >= 70){
+    console.log('Ami A Paise')
 }
-else if(bed > myBudget){
-        console.log('ami bed diye cha khabo');
+else if(myResult >= 50){
+    console.log('Ami A- Paise')
+}
+else if(myResult >= 33){
+    console.log('Ami B Paise')
 }
 else{
-    console.log('ami cha khamo na');
+    console.log('Ami Fail Korse 😢')
 }
