999ef8055d6d693df8e213d6c6c342ca3c07abfc
ibrahim Ahmed <bdibrahimahmed1231@gmail.com>
2022-02-06 07:12:03 +0600

    rivistion condision

--- condition/multiple-condition.js
+++ condition/multiple-condition.js
@@ -1,4 +1,4 @@
-var govJob = false;
+var govJob = true;
 var ghorVari = false;
 var monySave = 250000;
 
@@ -20,8 +20,15 @@ var monySave = 250000;
 //     console.log('ami biya kormo na');
 // }
 
-if (govJob == true || ghorVari == true || monySave > 200000){
-    console.log('Ami Biya kormo');
-}else{
-    console.log('ami biya kormo na');
+// if (govJob == true || ghorVari == true || monySave > 200000){
+//     console.log('Ami Biya kormo');
+// }else{
+//     console.log('ami biya kormo na');
+// }
+
+if((govJob == false && ghorVari == false) || monySave <= 10000){
+    console.log('ami biye kormo')
+}
+else{
+    console.log('ami biye kormo na')
 }
